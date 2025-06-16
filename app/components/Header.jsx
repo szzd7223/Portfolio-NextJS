019b944be4f8d1b1c154ab94ef26209d9aff0a4a
szzd7223 <mohdsaadshaikh17@gmail.com>
2025-06-16 14:15:05 +0530

    Portfolio-NextJS

---
+++ app/components/Header.jsx
@@ -0,0 +1,31 @@
+import { assets } from '@/assets/assets'
+import Image from 'next/image'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+
+      <div>
+        <Image src={assets.profile_img} alt='' className='rounded-full w-32'/>
+      </div>
+
+      <h3 className='flex items-end gap-2 text-xl md:text-2xl mb-3 font-ovo'>Hi! I'm Saad <Image src={assets.hand_icon} alt='' className='w-6'/></h3>
+
+      <h1 className='text-3xl sm:text-6xl lg:text-[66px] font-ovo'>Software Developer based in Bangalore</h1>
+
+      <p className='max-w-2xl mx-auto font-ovo'>
+        I am a software developer who enjoys building web applications and working on interesting projects. Always curious and learning, I like turning ideas into functional, clean code.
+      </p>
+
+      <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
+        <a href="#contact" className='px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2'>Contact me<Image src={assets.right_arrow_white} alt='' className='w-4'/></a>
+
+        <a href="/sample-resume.pdf" download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2'>My Resume<Image src={assets.download_icon} alt='' className='w-4'/></a>
+      </div>
+
+    </div>
+  )
+}
+
+export default Header
