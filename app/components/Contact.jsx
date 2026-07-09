c9e680be32973194a743dff542d337497110aeb6
szzd7223 <mohdsaadshaikh17@gmail.com>
2026-07-09 08:56:19 +0530

    update resume

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -4,85 +4,85 @@ import React, { useState } from 'react'
 import { motion } from "motion/react"
 
 const Contact = () => {
-    const [result, setResult] = useState('');
+  const [result, setResult] = useState('');
 
-    const onSubmit = async (event) => {
-        event.preventDefault();
-        setResult("Sending....");
-        const formData = new FormData(event.target);
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
 
-        formData.append("access_key", "9b1210b0-604f-45a1-8480-3706a65ae9ac");
+    formData.append("access_key", "9b1210b0-604f-45a1-8480-3706a65ae9ac");
 
-        const response = await fetch("https://api.web3forms.com/submit", {
-        method: "POST",
-        body: formData
-        });
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
 
-        const data = await response.json();
+    const data = await response.json();
 
-        if (data.success) {
-        setResult("Form Submitted Successfully");
-        event.target.reset();
-        } else {
-        console.log("Error", data);
-        setResult(data.message);
-        }
-    };
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
 
   return (
     <motion.div
-    initial={{opacity: 0}}
-    whileInView={{opacity: 1}}
-    transition={{duration: 1}}
-    id='contact' className='w-full px-[12%] py-10 scroll-mt-20 font-outfit bg-[url("/footer-bg-color.png") bg-no-repeat bg-center bg-[length:90%_auto] dark:bg-none'>
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+      id='contact' className='w-full px-[12%] py-10 scroll-mt-20 font-outfit bg-[url("/footer-bg-color.png") bg-no-repeat bg-center bg-[length:90%_auto] dark:bg-none'>
       <motion.h4
-      initial={{y: -20, opacity: 0}}
-    whileInView={{y:0, opacity: 1}}
-    transition={{delay:0.3, duration: 0.5}}
-      className='text-center mb-2 text-lg font-ovo'>Contact with me</motion.h4>
+        initial={{ y: -20, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ delay: 0.3, duration: 0.5 }}
+        className='text-center mb-2 text-lg font-ovo'>Contact with me</motion.h4>
       <motion.h2
-      initial={{y: -20, opacity: 0}}
-    whileInView={{y:0, opacity: 1}}
-    transition={{delay:0.5, duration: 0.5}}
-      className='text-center text-5xl font-ovo'>Get in touch</motion.h2>
+        initial={{ y: -20, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ delay: 0.5, duration: 0.5 }}
+        className='text-center text-5xl font-ovo'>Get in touch</motion.h2>
 
       <motion.p
-      initial={{opacity: 0}}
-    whileInView={{opacity: 1}}
-    transition={{delay:0.7, duration: 0.5}}
-      className='text-center max-w-2xl mx-auto mt-5 mb-12 font-ovo'>
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ delay: 0.7, duration: 0.5 }}
+        className='text-center max-w-2xl mx-auto mt-5 mb-12 font-ovo'>
         I’m currently open to full-time roles, internships, or freelance opportunities. If you’re hiring or have something exciting in mind, feel free to reach out — I’d love to connect and explore how I can contribute.
       </motion.p>
 
       <motion.form
-      initial={{opacity: 0}}
-    whileInView={{opacity: 1}}
-    transition={{delay:0.9, duration: 0.5}}
-      className='max-w-2xl mx-auto' onSubmit={onSubmit}>
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ delay: 0.9, duration: 0.5 }}
+        className='max-w-2xl mx-auto' onSubmit={onSubmit}>
         <div className='grid grid-cols-[repeat(auto-fit,_minmax(200px,_1fr))] gap-6 mt-10 mb-8'>
-            <motion.input
-            initial={{x: -50, opacity: 0}}
-            whileInView={{x:0, opacity: 1}}
-            transition={{delay:1.1, duration: 0.6}}
-            type="text" placeholder='Enter your name' required 
-            className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white dark:bg-darkHover/30 dark:border-white/90' name='name'/>
-            <motion.input
-            initial={{x: 50, opacity: 0}}
-            whileInView={{x:0, opacity: 1}}
-            transition={{delay:1.2, duration: 0.6}} type="email" placeholder='Enter your email' required 
-            className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white dark:bg-darkHover/30 dark:border-white/90' name='email'/>
+          <motion.input
+            initial={{ x: -50, opacity: 0 }}
+            whileInView={{ x: 0, opacity: 1 }}
+            transition={{ delay: 1.1, duration: 0.6 }}
+            type="text" placeholder='Enter your name' required
+            className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white dark:bg-darkHover/30 dark:border-white/90' name='name' />
+          <motion.input
+            initial={{ x: 50, opacity: 0 }}
+            whileInView={{ x: 0, opacity: 1 }}
+            transition={{ delay: 1.2, duration: 0.6 }} type="email" placeholder='Enter your email' required
+            className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white dark:bg-darkHover/30 dark:border-white/90' name='email' />
         </div>
         <motion.textarea
-            initial={{y: 100, opacity: 0}}
-    whileInView={{y:0, opacity: 1}}
-    transition={{delay:1.3, duration: 0.6}}
-        rows="6" placeholder='Enter your message' required 
-        className='w-full p-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-6 dark:bg-darkHover/30 dark:border-white/90' name='message'></motion.textarea>
+          initial={{ y: 100, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ delay: 1.3, duration: 0.6 }}
+          rows="6" placeholder='Enter your message' required
+          className='w-full p-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-6 dark:bg-darkHover/30 dark:border-white/90' name='message'></motion.textarea>
 
         <motion.button
-        whileHover={{scale: 1.05}}
-        transition={{duration: 0.3}}
-        type='submit' className='py-3 px-8 w-max flex items-center justify-between gap-2 bg-black/80 text-white rounded-full mx-auto hover:bg-black duration-500 cursor-pointer dark:bg-transparent dark:border-[0.5px] dark:hover:bg-darkHover'>Submit <Image src={assets.right_arrow_white} alt='' className='w-4'/></motion.button>
+          whileHover={{ scale: 1.05 }}
+          transition={{ duration: 0.3 }}
+          type='submit' className='py-3 px-8 w-max flex items-center justify-between gap-2 bg-black/80 text-white rounded-full mx-auto hover:bg-black duration-500 cursor-pointer dark:bg-transparent dark:border-[0.5px] dark:hover:bg-darkHover'>Submit <Image src={assets.right_arrow_white} alt='' className='w-4' /></motion.button>
 
         <p className='mt-4'>{result}</p>
       </motion.form>
--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -2,24 +2,25 @@ import { assets } from '@/assets/assets'
 import Image from 'next/image'
 import React from 'react'
 
-const Footer = ({isDarkMode}) => {
+const Footer = ({ isDarkMode }) => {
+  const year = new Date().getFullYear();
   return (
     <div className='mt-20'>
       <div className='text-center'>
-        <Image src={isDarkMode ? assets.logo_dark : assets.logo} alt='' className='w-40 mx-auto mb-2'/>
+        <Image src={isDarkMode ? assets.logo_dark : assets.logo} alt='' className='w-40 mx-auto mb-2' />
 
         <div className='w-max flex items-center gap-2 mx-auto font-outfit'>
-            <Image src={isDarkMode ? assets.mail_icon_dark : assets.mail_icon} alt='' className='w-6'/>
-            mohdsaadshaikh17@gmail.com
+          <Image src={isDarkMode ? assets.mail_icon_dark : assets.mail_icon} alt='' className='w-6' />
+          mohdsaadshaikh17@gmail.com
         </div>
       </div>
 
       <div className='font-outfit text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6'>
-        <p>© 2025 Saad Shaikh. All rights reserved</p>
+        <p>© {year} Saad Shaikh. All rights reserved</p>
         <ul className='flex items-center gap-10 justify-center mt-4 sm:mt-0'>
-            <li><a target='_blank' href="https://github.com/szzd7223">Github</a></li>
-            <li><a target='_blank' href="https://www.linkedin.com/in/ssaaaaddshaikh/">LinkedIn</a></li>
-            <li><a target='_blank' href="https://x.com/ssaaaadd_sh">Twitter</a></li>
+          <li><a target='_blank' href="https://github.com/szzd7223">Github</a></li>
+          <li><a target='_blank' href="https://www.linkedin.com/in/ssaaaaddshaikh/">LinkedIn</a></li>
+          <li><a target='_blank' href="https://x.com/ssaaaadd_sh">Twitter</a></li>
         </ul>
       </div>
     </div>
